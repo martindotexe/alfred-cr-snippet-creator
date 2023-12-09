5747b347344239cfc91ab0156677d328c346d011
martindotexe <martin@solhaugfoto.no>
2023-12-09 21:02:28 +0100

    bulk creation

--- main.py
+++ main.py
@@ -8,35 +8,31 @@ import json
 RESULT_COLLECTION_NAME = "My New Snippet Collection"
 
 
-def build_json_files():
-    source_file = "snippets.txt"
-
-    with open(source_file, newline='', encoding="utf-8-sig") as file:
-        rows = [line.split("\t") for line in file.readlines()]
-        for row in rows:
-            uid = token_hex(15)
-            output = json.dumps(
-                {
-                    "alfredsnippet": {
-                        "snippet": row[1],
-                        "uid": uid,
-                        "keyword": row[0],
-                        "name": row[1],
-                    },
+def build_json_files(file_name, values):
+    for row in values:
+        uid = token_hex(15)
+        output = json.dumps(
+            {
+                "alfredsnippet": {
+                    "snippet": row[1],
+                    "uid": uid,
+                    "keyword": row[0],
+                    "name": row[1],
                 },
-                sort_keys=False,
-                indent=4,
-                separators=(',', ': '),
-            )
-            output_file = RESULT_COLLECTION_NAME + \
-                "/" + row[1].replace("/", "") + " [" + uid + "].json"
-            with open(output_file, "w") as f:
-                f.write(output)
+            },
+            sort_keys=False,
+            indent=4,
+            separators=(',', ': '),
+        )
+        output_file = file_name + \
+            "/" + row[1].replace("/", "") + " [" + uid + "].json"
+        with open(output_file, "w") as f:
+            f.write(output)
 
 
-def zip_files():
-    with zipfile.ZipFile(RESULT_COLLECTION_NAME + ".zip", "w") as zf:
-        for root, _, files in walk(RESULT_COLLECTION_NAME):
+def zip_files(file_name):
+    with zipfile.ZipFile(file_name + ".zip", "w") as zf:
+        for root, _, files in walk(file_name):
             for f in files:
                 zf.write(
                     path.join(root, f),
@@ -45,15 +41,29 @@ def zip_files():
                 )
 
 
-def change_zip_extension():
-    renamee = RESULT_COLLECTION_NAME + ".zip"
+def change_zip_extension(file_name):
+    renamee = file_name + ".zip"
     pre, _ = path.splitext(renamee)
     rename(renamee, pre + ".alfredsnippets")
 
 
-mkdir(RESULT_COLLECTION_NAME)
-copyfile("./info.plist", "./" + RESULT_COLLECTION_NAME + "/info.plist")
-build_json_files()
-zip_files()
-change_zip_extension()
-rmtree(RESULT_COLLECTION_NAME)
+def collect_files():
+    data = {}
+    for root, _, files in walk("files"):
+        for file in files:
+            if file.endswith(".txt"):
+                with open(f"{root}/{file}") as f:
+                    data[file] = [tuple(line.strip().split("\t"))
+                                  for line in f.readlines()]
+    return data
+
+
+file_data = collect_files()
+for key, values in file_data.items():
+    file_name = key.replace(".txt", "")
+    mkdir(file_name)
+    copyfile("./info.plist", "./" + file_name + "/info.plist")
+    build_json_files(file_name, values)
+    zip_files(file_name)
+    change_zip_extension(file_name)
+    rmtree(file_name)
